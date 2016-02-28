423fe28f916153499366209422df6b5fc9464067
Terrence Reilly <terrencepreilly@gmail.com>
2016-02-28 07:30:51 -0500

    Ported to Python3, added PEP8 formatting.

--- ProblemSetManager.py
+++ ProblemSetManager.py
@@ -2,145 +2,134 @@ from ProblemSet import ProblemSet
 import math
 import random
 
+
 class ProblemSetManager(object):
-	"""Loads ProblemSets and performs operations on them."""
+    """Loads ProblemSets and performs operations on them."""
 
-	def __init__(self, filename="Stats_Problems.txt"):
-		"""Return a ProblemSetManager.
-		
-		Keyword Arguments
-		filename -- The name of the file containing ProblemSet 
-                    definitions.
-		"""
-		self.filename = filename
-		self.problem_sets = [a for a in self.load_problems()]
-		self.rand = random.Random()
+    def __init__(self, filename="Stats_Problems.txt"):
+        """Return a ProblemSetManager.
 
-	def load_problems(self):
-		"""Load the ProblemSets defined in the filename."""
-		fin = open(self.filename, 'r')
-		lines = [a.strip() for a in fin.readlines()]
-		key = lines[0]
-		lines = lines[1:]
+        Keyword Arguments
+        filename -- The name of the file containing ProblemSet
+             definitions.
+        """
+        self.filename = filename
+        self.problem_sets = [a for a in self.load_problems()]
+        self.rand = random.Random()
 
-		for line in lines:
-			ps = ProblemSet()
-			ps.init_line(line, key)
-			yield ps
+    def load_problems(self):
+        """Load the ProblemSets defined in the filename."""
+        fin = open(self.filename, 'r')
+        lines = [a.strip() for a in fin.readlines()]
+        key = lines[0]
+        lines = lines[1:]
 
-		fin.close()
+        for line in lines:
+            ps = ProblemSet()
+            ps.init_line(line, key)
+            yield ps
 
-	def sort(self):
-		"""Sorts the ProblemSets using the compare method 
-                ProblemSet.__cmp__
-                """
-		self.problem_sets.sort()
+        fin.close()
 
-	def sort_by_quotient(self):
-		"""Sorts problems according to ProblemSet.quotient, which is 
+    def sort(self):
+        """Sorts the ProblemSets using the compare method
+        ProblemSet.__cmp__
+        """
+
+        self.problem_sets.sort()
+
+    def sort_by_quotient(self):
+        """Sorts problems according to ProblemSet.quotient, which is
                 right - wrong.
-                """
-		self.problem_sets = [ (-1*a.quotient(), a) for a in self.problem_sets]
-		self.problem_sets.sort()
-		self.problem_sets = [a[1] for a in self.problem_sets]
+         """
+        self.problem_sets = [(-1*a.quotient(), a) for a in self.problem_sets]
+        self.problem_sets.sort()
+        self.problem_sets = [a[1] for a in self.problem_sets]
 
-	def index(self, chapter, section):
-		"""Return the index of this chapter and section.
-		
-		Keyword arguments:
-		chapter -- The chapter of the ProblemSet wanted.
-		section -- The section of the problemSet wanted.
-		"""
-		return self.problem_sets.index( ProblemSet(chapter, section) )
+    def index(self, chapter, section):
+        """Return the index of this chapter and section.
 
-	def mark_wrong(self, chapter, section):
-		"""Increment the wrong counter on the designated ProblemSet.
+        Keyword arguments:
+        chapter -- The chapter of the ProblemSet wanted.
+        section -- The section of the problemSet wanted.
+        """
+        return self.problem_sets.index(ProblemSet(chapter, section))
 
-		Keyword arguments:
-		chapter -- The chapter of the ProblemSet wanted.
-		section -- The section of the problemSet wanted.
-		"""
-		self.problem_sets[ self.index(chapter, section) ].mark_wrong()
+    def mark_wrong(self, chapter, section):
+        """Increment the wrong counter on the designated ProblemSet.
 
-	def mark_right(self, chapter, section):
-                """Increment the wrong counter on the designated ProblemSet.
+        Keyword arguments:
+        chapter -- The chapter of the ProblemSet wanted.
+        section -- The section of the problemSet wanted.
+        """
+        self.problem_sets[self.index(chapter, section)].mark_wrong()
 
-                Keyword arguments:
-                chapter -- The chapter of the ProblemSet wanted.
-                section -- The section of the problemSet wanted.
-                """
-		self.problem_sets[ self.index(chapter, section) ].mark_right()
+    def mark_right(self, chapter, section):
+        """Increment the wrong counter on the designated ProblemSet.
 
-	def weighted_num(self, mu, sigma):
-		"""Return a random, weighted number normally distributed 
+        Keyword arguments:
+        chapter -- The chapter of the ProblemSet wanted.
+        section -- The section of the problemSet wanted.
+        """
+        self.problem_sets[self.index(chapter, section)].mark_right()
+
+    def weighted_num(self, mu, sigma):
+        """Return a random, weighted number normally distributed
                 around mu with a standard deviation of sigma.
 
-		Keyword arguments:
-		mu -- The average value of the variable.
-		sigmam -- The standard deviation of the distribution.
-		"""
-		return int( math.log( self.rand.lognormvariate(mu, sigma) ) )
+        Keyword arguments:
+        mu -- The average value of the variable.
+        sigmam -- The standard deviation of the distribution.
+        """
+        return int(math.log(self.rand.lognormvariate(mu, sigma)))
 
-	def trimmed_weighted_num(self, mu, sigma, minimum, maximum):
-		"""Return a random, weighted number normally distributed 
-                around mu with a standard deviation of sigma, trimmed to be 
+    def trimmed_weighted_num(self, mu, sigma, minimum, maximum):
+        """Return a random, weighted number normally distributed
+                around mu with a standard deviation of sigma, trimmed to be
                 between minimum and maximum.
 
-		Keyword arguments:
-		mu -- The average value of the variable.
-		sigma -- The standard deviation of the distribution.
-		minimum -- The smallest value the variable can take.
-		maximum -- The largest value the variable can take.
-		"""
-		x = self.weighted_num(mu, sigma)
-		if x < minimum or x > maximum: 
-			return self.trimmed_weighted_num(mu, sigma, minimum, maximum)
-		return x
+        Keyword arguments:
+        mu -- The average value of the variable.
+        sigma -- The standard deviation of the distribution.
+        minimum -- The smallest value the variable can take.
+        maximum -- The largest value the variable can take.
+        """
+        x = self.weighted_num(mu, sigma)
+        if x < minimum or x > maximum:
+            return self.trimmed_weighted_num(mu, sigma, minimum, maximum)
+        return x
 
-	def random_problem_set_weighted(self):
-		"""Return a random problem set, weighted by its position in 
+    def random_problem_set_weighted(self):
+        """Return a random problem set, weighted by its position in
                 the ProblemSet list.
                 """
-		minimum = 0
-		maximum = len(self.problem_sets) - 1
-		mu = .8 * maximum 
-		sigma = .3 * maximum
-                pnum = self.trimmed_weighted_num( mu, sigma, minimum, maximum)
-		return self.problem_sets[ int(pnum) ]
+        minimum = 0
+        maximum = len(self.problem_sets) - 1
+        mu = .8 * maximum
+        sigma = .3 * maximum
+        pnum = self.trimmed_weighted_num(mu, sigma, minimum, maximum)
+        return self.problem_sets[int(pnum)]
 
-	def save_problems(self, ofilename="save.txt"):
-		"""Save a string representation of the ProblemSets.
+    def save_problems(self, ofilename="save.txt"):
+        """Save a string representation of the ProblemSets.
 
-		Keyword arguments:
-		ofilename -- The name of the output file.
-		"""
-		fout = open(ofilename, 'w')
-		fout.write("chapter\tsection\tproblems\tpage\tright\twrong\n")
-		for ps in self.problem_sets:
-			fout.write(str(ps) + '\n')
-		fout.close()
+        Keyword arguments:
+        ofilename -- The name of the output file.
+        """
+        fout = open(ofilename, 'w')
+        fout.write("chapter\tsection\tproblems\tpage\tright\twrong\n")
+        for ps in self.problem_sets:
+            fout.write(str(ps) + '\n')
+        fout.close()
 
-	def get_headers(self):
-                """Get the header string."""
-		return "ch.\tsect.\tprob.\tpage\tright\twrong"
+    def get_headers(self):
+        """Get the header string."""
+        return 'ch.\tsect.\tprob.\tpage\tright\twrong'
 
-        def get_stats(self):
-                """Return a dictionary containing descriptive
-                statistics.
-                """
-                d = dict()
-                d['right'] = reduce(lambda x, y: x + y.right, 
-                                    self.problem_sets, 0)
-                d['wrong'] = reduce(lambda x, y: x + y.wrong, 
-                                    self.problem_sets, 0)
-                d['total'] = d['right'] + d['wrong']
-                return d
-
-if __name__=="__main__":
-	#psm = ProblemSetManager("Algebra_Problems.txt")
-	psm = ProblemSetManager("Stats_Problems.txt")
-	psm.sort_by_quotient()
-	print "ch.\tsect.\tprob.\tpage\tright\twrong"
-	for i in range(14):
-		print psm.random_problem_weighted().str_rand_problem()
-	
+    def get_stats(self):
+        """Return a dictionary containing descriptive statistics. """
+        d = dict()
+        d['right'] = sum(map(lambda x: x.right, self.problem_sets))
+        d['wrong'] = sum(map(lambda x: x.wrong, self.problem_sets))
+        d['total'] = d['right'] + d['wrong']
+        return d
--- UpdateProblems.py
+++ UpdateProblems.py
@@ -4,98 +4,99 @@ import datetime
 
 parser = argparse.ArgumentParser(description="Handle Reviewing Operations")
 
-parser.add_argument('-f', nargs='?', help="""The filename for the book being 
+parser.add_argument('-f', nargs='?', help="""The filename for the book being
                     reviewed.""")
-parser.add_argument('-c', nargs='*', help="""The problem gotten correct in 
-                    the format chapter.section:problem.  For example, 6.5:15 
+parser.add_argument('-c', nargs='*', help="""The problem gotten correct in
+                    the format chapter.section:problem.  For example, 6.5:15
                     would be problem 15 from section 5 of chapter 6.""")
-parser.add_argument('-i', nargs='*', help="""The problem gotten incorrect in 
-                    the format chapter.section:problem.  For example, 6.5:15 
+parser.add_argument('-i', nargs='*', help="""The problem gotten incorrect in
+                    the format chapter.section:problem.  For example, 6.5:15
                     would be problem 15 from section 5 of chapter 6.""")
-parser.add_argument('--set-default', nargs='?', help="""Set the default file 
+parser.add_argument('--set-default', nargs='?', help="""Set the default file
                     to work on.""")
-parser.add_argument('-t', action='store_true', help="""Adds a timestamp to 
-                    the filename. (Note: the file name must include some 
+parser.add_argument('-t', action='store_true', help="""Adds a timestamp to
+                    the filename. (Note: the file name must include some
                     extension, e.g.: .txt)""")
-parser.add_argument('-r', nargs='?', help="""Generate R random problems, 
+parser.add_argument('-r', nargs='?', help="""Generate R random problems,
                     weighted.""")
-parser.add_argument('-p', action='store_true', help="""Print the list of 
+parser.add_argument('-p', action='store_true', help="""Print the list of
                     problem sets.""")
-parser.add_argument('--statistics', action='store_true', help="""Display 
+parser.add_argument('--statistics', action='store_true', help="""Display
                     review statistics on the given file.""")
 args = parser.parse_args()
 
-#-----------------------------------------------------------------#
-#----------------SET THE DEFAULT FILENAME-------------------------#
+# --------------------------------------------------------------- #
+# ---------------SET THE DEFAULT FILENAME------------------------ #
 if args.set_default:
     fin = open(".reviewerDefault", 'w')
     fin.write(args.set_default)
     fin.close()
 
-#----------------GET THE FILENAME---------------------------------#
+# ---------------GET THE FILENAME-------------------------------- #
 filename = None
 if args.f:
     filename = args.f
 else:
-    fin = open(".reviewerDefault", 'r')
-    filename = fin.readline().strip()
-    fin.close()
+    with open(".reviewerDefault", 'r') as fin:
+        filename = fin.readline().strip()
 
-#----------------OPEN PROBLEMSETMANAGER---------------------------#
+# ---------------OPEN PROBLEMSETMANAGER-------------------------- #
 psm = ProblemSetManager(filename) if filename is not None else None
 timestamp = str(datetime.date.today())
+
 
 def splitproblem(s):
     return s.split(':')[0].split('.')
 
-#----------------MARK CORRECT-------------------------------------#
+# ---------------MARK CORRECT------------------------------------ #
 if args.c:
     for p in args.c:
         try:
             prob = splitproblem(p)
-            psm.mark_right( int(prob[0]), int(prob[1]) )
+            psm.mark_right(int(prob[0]), int(prob[1]))
         except:
-            print "Problem not in problem set!"
+            print("Problem not in problem set!")
 
-#----------------MARK INCORRECT-----------------------------------#
+# ---------------MARK INCORRECT---------------------------------- #
 if args.i:
     for p in args.i:
         try:
             prob = splitproblem(p)
-            psm.mark_wrong( int(prob[0]), int(prob[1]) )
+            psm.mark_wrong(int(prob[0]), int(prob[1]))
         except:
-            print "Problem not in problem set!"
+            print("Problem not in problem set!")
 
-#----------------SORT, SAVE, AND CLOSE----------------------------#
-if psm is not None and (args.i or args.c): # only save if marking 
+# ---------------SORT, SAVE, AND CLOSE--------------------------- #
+if psm is not None and (args.i or args.c):  # only save if marking
     psm.sort_by_quotient()
     if args.c or args.i:
         if args.t:
-            psm.save_problems(filename.split('.')[0] + '_' + timestamp + '.' + filename.split('.')[1])
+            psm.save_problems(filename.split('.')[0] + '_' +
+                              timestamp + '.' + filename.split('.')[1])
         else:
             psm.save_problems(filename)
 
-#----------------PRINT RANDOM PROBLEMS----------------------------#
+# ---------------PRINT RANDOM PROBLEMS--------------------------- #
 if args.r and psm is not None:
-    print psm.get_headers()
-    for i in range( int(args.r) ):
+    print(psm.get_headers())
+    for i in range(int(args.r)):
         rand_prob_set = psm.random_problem_set_weighted()
-        print rand_prob_set.str_rand_problem()
+        print(rand_prob_set.str_rand_problem())
 
 
-#----------------PRINT ALL PROBLEM SETS---------------------------#
+# ---------------PRINT ALL PROBLEM SETS-------------------------- #
 if args.p and psm is not None:
-    print psm.get_headers()
+    print(psm.get_headers())
     for ps in psm.problem_sets:
-        print ps
+        print(ps)
 
-#----------------DISPLAY DESCRIPTIVE STATISTICS-------------------#
+# ---------------DISPLAY DESCRIPTIVE STATISTICS------------------ #
 if args.statistics and psm is not None:
     stats = psm.get_stats()
     p = float(stats['right']) / float(stats['total']) * 100
-    print '\n#--------DESCRIPTIVE STATISTICS FOR ' + psm.filename + '-----------#'
-    print 'Total Reviewed: %d\t' % stats['total'],
-    print 'Percent Right: ' + str(p)[:4] + '%',
-    print 'Total Correct: %d' % stats['right'],
-    print 'Total Incorrect: %d' % stats['wrong']
-    print
+    print('\n#--------DESCRIPTIVE STATISTICS FOR ' + psm.filename +
+          '-----------#')
+    print('\tTotal Reviewed:\t\t%d' % stats['total'])
+    print('\tPercent Right:\t\t' + str(p)[:4] + '%')
+    print('\tTotal Correct:\t\t%d' % stats['right'])
+    print('\tTotal Incorrect:\t%d' % stats['wrong'])
